5efece975cbeceb1da3472eefad3d38b4120be82
hyho942 <hyunholee90@gmail.com>
2019-07-15 14:39:47 +0900

    ADD: html/css

--- src/app/user/components/order-list.component.ts
+++ src/app/user/components/order-list.component.ts
@@ -53,7 +53,66 @@ import { UserService } from 'src/app/core/user.service';
     </div>
     <app-footer></app-footer>
   `,
-  styles: []
+  styles: [
+    `
+      /* order-list */
+      body {
+        font-family: 'Noto Sans KR', 'Apple SD Gothic Neo', '맑은 고딕';
+        letter-spacing: -0.4px;
+        background-color: antiquewhite;
+      }
+      .shopping_order_list {
+        background-color: chartreuse;
+        width: 1136px;
+        margin-right: auto;
+        margin-left: auto;
+        text-align: center;
+      }
+      .my_mileage {
+        background-color: fuchsia;
+        height: 80px;
+        display: grid;
+        padding: 15px 0;
+        box-sizing: border-box;
+        grid-template-columns: 1fr 1fr 1fr;
+      }
+      .mileage_item {
+        display: inline-block;
+        line-height: 50px;
+        border: 1px solid black;
+      }
+      .slot {
+        background-color: darkviolet;
+        width: 1136px;
+        text-align: center;
+        margin-top: 30px;
+        margin-right: auto;
+        margin-left: auto;
+        display: grid;
+        grid-template-columns: 1fr 2fr;
+      }
+      .slot_item {
+        border: 1px solid black;
+        line-height: 80px;
+        display: inline-block;
+      }
+      .order_state {
+        background-color: gold;
+        margin-top: 30px;
+        width: 1136px;
+        padding: 0 110px;
+        box-sizing: border-box;
+        margin-right: auto;
+        margin-left: auto;
+        display: flex;
+      }
+      .step {
+        text-align: center;
+        display: inline-block;
+        justify-content: space-around;
+      }
+    `
+  ]
 })
 export class OrderListComponent implements OnInit {
   constructor(private userService: UserService) {}
