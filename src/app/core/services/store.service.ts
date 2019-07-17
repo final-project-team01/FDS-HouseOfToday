0229e9de98563e07d4f89f44f381568389bf62a4
hicucu <hicucu@gmail.com>
2019-07-17 10:50:38 +0900

    add services

--- src/app/core/services/store.service.ts
+++ src/app/core/services/store.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 
 import { CoreModule } from '../core.module';
+import { ServiceCore } from '../serviceCore';
+import { store_list } from '../models/store.interface';
 
 @Injectable({
   providedIn: CoreModule
@@ -9,4 +11,11 @@ import { CoreModule } from '../core.module';
 export class StoreService {
 
   constructor(private httpClient: HttpClient) { }
-}
+
+  getCategoryList() {
+    const path = "products/category/list/"
+    const fullPath = ServiceCore.getFullPath(path);
+
+    return this.httpClient.get<store_list[]>(fullPath);
+  }
+} 
