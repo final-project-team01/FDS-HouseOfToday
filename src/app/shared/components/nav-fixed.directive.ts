01eaa638118566d4f52088342d023d1dce718e12
hicucu <hicucu@gmail.com>
2019-07-21 16:28:15 +0900

    feat: main nav fixed 마우스 스크롤시 메인 네비게이션 fixed

---
+++ src/app/shared/components/nav-fixed.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[NavFixed]'
+})
+export class NavFixedDirective {
+
+  constructor(public el: ElementRef, public renderer: Renderer2) { }
+
+  @HostListener("window:scroll") scrollHandler() {
+    if (window.pageYOffset > 0)
+      this.toggleNavFixed(true);
+    else this.toggleNavFixed(false);
+    console.log("!");
+
+  }
+
+  toggleNavFixed(sticky: boolean) {
+    this.renderer.setStyle(
+      this.el.nativeElement,
+      "position", sticky ? "fixed" : "relative"
+    );
+  }
+}
