7717068b00e62446abbf12a250ed05a6b0b299b7
dearjsmc4 <dearjsmc4@gmail.com>
2019-07-21 22:09:52 +0900

    Feat: Add review filter menus

--- src/app/store/components/store-detail-components/product-review.component.ts
+++ src/app/store/components/store-detail-components/product-review.component.ts
@@ -6,14 +6,26 @@ import { HttpClient } from '@angular/common/http';
   template: `
     <div class="product-review-container">
       <h3>리뷰 <span class="review-amount" *ngIf="reviews">{{ reviews.length }}</span></h3>
-      <a href="">리뷰쓰기</a>
-      <ul class="review-filter">
+      <a href="" class="write-review">리뷰쓰기</a>
+      <div class="filter-container">
+      <ul>
         <li class="review-filter-menu">베스트순</li>
         <li class="review-filter-menu">최신순</li>
         <li class="review-filter-menu">사진리뷰</li>
-        <li class="review-filter-menu">별점</li>
-        <li class="review-filter-menu">옵션</li>
       </ul>
+      <ul class="filter-btns">
+        <li>
+          <button class="review-filter-btn">별점
+          <span class="filter-icon"></span>
+          </button>
+        </li>
+        <li>
+          <button class="review-filter-btn">옵션
+          <span class="filter-icon"></span>
+          </button>
+        </li>
+      </ul>
+      </div>
       <article class="user-review" *ngFor="let review of chosenReviews">
         <span>사용자</span>
         <div class="review-star-score">
@@ -42,6 +54,69 @@ import { HttpClient } from '@angular/common/http';
     width: 690px;
     background-color: pink;
     padding: 60px 30px 30px 30px;
+    position: relative;
+  }
+  h3{
+    font-size: 18px;
+    font-weight: 700;
+    color: #000;
+  }
+  h3 > span{
+    margin-left: 6px;
+    font-size: 18px;
+    font-weight: 700;
+    color: #35c5f0;
+  }
+  .write-review{
+    position: absolute;
+    color: #35c5f0;
+    top: 60px;
+    right: 30px;
+    border: none;
+    font-weight: 700;
+  }
+  .filter-container{
+    background-color: skyblue;
+    margin: 15px 0;
+    padding: 15px 15px;
+    border-top: 1px solid lightgrey;
+    border-bottom: 1px solid lightgrey;
+    position: relative;
+  }
+  .review-filter-menu{
+    display: inline-block;
+    cursor: pointer;
+    margin-right: 12px;
+    font-size: 13px;
+    font-weight: 700;
+    line-height: 13px;
+    color: #757575;
+  }
+  .filter-btns{
+    position: absolute;
+    top: 11px;
+    right: 15px;
+  }
+  .filter-btns > li{
+    display: inline-block;
+    margin-left: 5px;
+  }
+  .review-filter-btn{
+    font-size: 15px;
+    line-height: 19px;
+    padding: 7px 8px 6px;
+    border-style: none;
+    border-radius: 4px;
+    background-color: #f5f5f5;
+    border-color: #f5f5f5;
+    color: #757575;
+    cursor: pointer;
+  }
+  .filter-icon{
+    display: inline-block;
+    width: 12px;
+    height: 12px;
+    background-position: -387px 3px;
   }
   .user-review{
     padding: 30px 0;
@@ -95,7 +170,7 @@ import { HttpClient } from '@angular/common/http';
     cursor: pointer;
     margin: 5px;
   }
-  .pagination-btn{
+  .pagination-btn, .filter-icon{
     background-image: url('../../../../assets/image/icon-pointer.png');
   }
   .left{
@@ -115,14 +190,13 @@ export class ProductReviewComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get('http://52.78.112.247/products/product/4/')
+    this.http.get('http://52.78.112.247/products/product/1/')
       .subscribe((detail) => {
         this.reviews = detail['review'];
         this.chosenReviews = this.reviews.filter((review, index) => index >= 0 && index < 3);
         const i = Math.ceil(this.reviews.length / 3);
         this.pages = Array(i);
       });
-
   }
 
   range(i: number){
@@ -132,7 +206,8 @@ export class ProductReviewComponent implements OnInit {
   changePage(i: number){
     const start = i * 3;
     const end = start + 3;
-    this.chosenReviews = this.reviews.filter((review, index) => index >= start && index < end);
+    this.chosenReviews 
+      = this.reviews.filter((review, index) => index >= start && index < end);
   }
 
 }
