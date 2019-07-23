4e7b23b845411504aef8fa9ad44964402d053f83
hicucu <hicucu@gmail.com>
2019-07-23 19:31:44 +0900

    feat: user 메뉴 nav 링크 적용
    user 페이지에서 nav메뉴에 링크

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,6 +44,7 @@ export class AppComponent {
   getUserDetail() {
     this.userService.getUserDetail().subscribe(req => {
       this.commonService.setUserDetail(req[0]);
+      this.commonService.setId(this.commonService.getUserDetail()["id"]);
     });
   }
 
