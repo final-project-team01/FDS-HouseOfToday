0ef2f7036ae5ac7fea70a4330fe703c9a9412dd9
dearjsmc4 <dearjsmc4@gmail.com>
2019-07-24 18:28:33 +0900

    Feat: pagination through numbers

--- src/app/store/components/store-detail-components/pagination.component.ts
+++ src/app/store/components/store-detail-components/pagination.component.ts
@@ -6,7 +6,7 @@ import { CommonService } from 'src/app/core/services/common.service';
   template: `
   <div class="pagination">
     <button class="pagination-btn left"></button>
-    <div class="numbers-container">
+    <div class="numbers-container" [style.width.px]="getWidth(pages)">
       <div class="btn-container" [style.left.px]="left">
         <button *ngFor="let page of pages; let i = index"
           class="pagination-page"
@@ -28,7 +28,7 @@ import { CommonService } from 'src/app/core/services/common.service';
     display: inline-block;
     white-space: nowrap;
     overflow-x: hidden;
-    width: 195px;
+    width: 0;
     vertical-align: middle;
     position: relative;
     height: 40px;
@@ -81,19 +81,25 @@ export class PaginationComponent implements OnInit {
   ngOnInit() {
   }
 
+  getWidth(pages: any){
+    if (pages.length >= 5) return 200;
+    else return pages.length * 40;
+  }
+
   changePage(i: number, chosenList, originalList){
     chosenList = this.commonService.changePage(i, chosenList, originalList);
     this.change.emit(chosenList);
     this.activeId = i;
     const diff = i - this.previousIndex;
     if (i >= 3) {
-      if (diff >= 3) this.left -= 40;
-      else if (diff > 0) this.left -= 40 * diff;
-      else if (diff < 0) this.left -= 40 * diff;
+      if (diff >= 3) this.left -= 40 * (diff - 2);
+      else if(this.previousIndex === 1 && diff >= 2) 
+        this.left -= 40 * (diff - 1)
+      else if (diff > 0 || diff < 0) this.left -= 40 * diff;
     }
     else if(i <= 2){
-      if (i === 2 && diff === -1) this.left += 40;
-      else if (i === 1 && diff === -1) return;
+      if (i === 2 && diff <= -1) this.left -= 40 * diff;
+      else if (i === 1 && diff <= -2) this.left -= 40 * (diff + 1);
     }
     this.previousIndex = i;
   }
