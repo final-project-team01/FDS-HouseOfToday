78d2140b1ac64e73baec0d51e4a29e993ea4eaaf
dearjsmc4 <dearjsmc4@gmail.com>
2019-07-26 12:10:11 +0900

    Feat: Fix pagination
    There was a bug, so I fixed it
    And simplified the code.

--- src/app/store/components/store-detail-components/pagination.component.ts
+++ src/app/store/components/store-detail-components/pagination.component.ts
@@ -87,9 +87,12 @@ export class PaginationComponent implements OnInit {
   }
 
   goLeft(){
+    // 1 일때 스탑
     if (this.activeId === 0) return;
+    // 3 아래로는 버튼색만 바뀌게
     else if (this.activeId <= 2) {
       this.activeId -= 1;
+    // 3 이상은 왼쪽으로 이동 + 버튼색 바뀜
     } else if (this.activeId >= 3) {
       this.activeId -= 1;
       this.left += 40;
@@ -99,9 +102,12 @@ export class PaginationComponent implements OnInit {
   }
 
   goRight(){
+    // 맨 마지막으로 가면 스탑
     if (this.activeId === this.pages.length - 1) return;
+    // 3 아래로는 버튼색만 바뀌게
     else if (this.activeId <= 1) {
       this.activeId += 1;
+    // 3 이상은 오른쪽으로 이동 + 버튼색 바뀜
     } else if (this.activeId >= 2) {
       this.activeId += 1;
       this.left -= 40;
@@ -113,16 +119,16 @@ export class PaginationComponent implements OnInit {
   changePage(i: number){
     this.getChosenList(i);
     this.activeId = i;
+    // 페이지 이동 간격 체크하는 변수
     const diff = i - this.previousIndex;
+    // 클릭한 페이지가 4 이상일 때
     if (i >= 3) {
-      if (diff >= 3) this.left -= 40 * (diff - 2);
-      else if(this.previousIndex === 1 && diff >= 2) 
-        this.left -= 40 * (diff - 1)
+      // 1,2 에서 이동했을 때
+      if (this.previousIndex <= 1 && diff >= 2) this.left -= 40 * (i - 2);
+      // 그 외에는 페이지 이동간격만큼 움직이도록
       else if (diff > 0 || diff < 0) this.left -= 40 * diff;
-    } else if(i <= 2){
-      if (i === 2 && diff <= -1) this.left -= 40 * diff;
-      else if (i === 1 && diff <= -2) this.left -= 40 * (diff + 1);
-    }
+    // 5 또는 4 에서 1,2로 바로 이동할 때
+    } else if(i <= 2 && diff <= -2) this.left += 40 * i;
     this.previousIndex = i;
   }
 
