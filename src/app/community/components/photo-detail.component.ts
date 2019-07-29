49b8145072cc639b21cb78106634b5bb3d943fda
hicucu <hicucu@gmail.com>
2019-07-29 19:21:24 +0900

    feat: community page add
    사진디테일, 집들이, 집들이디테일 페이지 추가

---
+++ src/app/community/components/photo-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-detail',
+  template: `
+    <p>
+      photo-detail works!
+    </p>
+  `,
+  styles: []
+})
+export class PhotoDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
