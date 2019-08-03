b5416eb0b3e4e702a6fe9b2ae3030a773c908bbc
Mark Yoo <yjinhyuk@gmail.com>
2019-08-03 21:56:05 +0900

    Feat: community home minor fix

--- src/app/core/services/community.service.ts
+++ src/app/core/services/community.service.ts
@@ -2,11 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { CoreModule } from '../core.module';
+import { CommonService } from './common.service';
 
 @Injectable({
   providedIn: CoreModule
 })
 export class CommunityService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private commonService: CommonService) { }
+
+  getCommunityHome() {
+    const path = 'community/home/';
+    const fullpath = this.commonService.getFullPath(path);
+    return this.httpClient.get(fullpath);
+  }
 }
