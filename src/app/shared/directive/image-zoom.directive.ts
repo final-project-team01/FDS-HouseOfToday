e0c0cc216bc2fb990b782a2a70ad4d276dd8d347
hicucu <hicucu@gmail.com>
2019-08-05 17:03:36 +0900

    feat: modify imagezoom
    imageZoom directive 수정

--- src/app/shared/directive/image-zoom.directive.ts
+++ src/app/shared/directive/image-zoom.directive.ts
@@ -8,10 +8,13 @@ export class ImageZoomDirective {
   constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   @HostListener("mouseover") mouseoverHandler() {
-    this.renderer.setStyle(this.el.nativeElement.querySelector('.image-zoom'), 'transform', 'scale(1.05) translate(-50%, -50%)');
+    this.renderer.setStyle(this.el.nativeElement.querySelector('.image-zoom'), 'transform', 'scale(1.05) translate(0%, 0%)');
+    this.renderer.setStyle(this.el.nativeElement.querySelector('.image-zoom'), 'transition', 'transform 0.3s ease');
+
   }
   @HostListener("mouseout") mouseoutHandler() {
-    this.renderer.setStyle(this.el.nativeElement.querySelector('.image-zoom'), 'transform', 'scale(1) translate(-50%, -50%)');
+    this.renderer.setStyle(this.el.nativeElement.querySelector('.image-zoom'), 'transform', 'scale(1) translate(0%, 0%)');
+    this.renderer.setStyle(this.el.nativeElement.querySelector('.image-zoom'), 'transition', 'transform 0.3s ease');
   }
 
 }
