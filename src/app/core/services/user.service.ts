60df2cfb99d14f0f8de7174c5633ed0a0badd74c
hicucu <hicucu@gmail.com>
2019-08-07 17:47:57 +0900

    feat:유저정보 수정
    유저정보 수정

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http'
 import { CoreModule } from '../core.module';
 import { CommonService } from './common.service';
 import { AuthService } from './auth.service';
-import { user_detail, user_order } from '../models/user.interface';
+import { user_detail, user_order, account_update, account_update_payload } from '../models/user.interface';
 
 @Injectable({
   providedIn: CoreModule
@@ -18,7 +18,7 @@ export class UserService {
     , private http: HttpClient
   ) { }
 
-  getUserDetail() {
+  getUser() {
     const headers = this.commonService.setAuthorization(this.commonService.Token);
     const path = "accounts/list/";
     const fullPath = this.commonService.getFullPath(path);
@@ -31,5 +31,22 @@ export class UserService {
     const path = "products/order/";
     const fullPath = this.commonService.getFullPath(path);
     return this.http.get<user_order[]>(fullPath, { headers });
+  }
+
+  patchAccountsUpdate(payload: account_update_payload) {
+    const headers = this.commonService.setAuthorization(this.commonService.Token);
+    const path = `accounts/update/${this.commonService.getUserDetail()['id']}/`;
+
+    const fullPath = this.commonService.getFullPath(path);
+    const options = { headers }
+    this.http.patch<account_update[]>(fullPath, payload, options).subscribe(
+      res => {
+        this.getUser().subscribe((user) => {
+          console.log('user', user);
+          this.commonService.setUserDetail(user[0]);
+        });
+      },
+      error => { }
+    );
   }
 }
