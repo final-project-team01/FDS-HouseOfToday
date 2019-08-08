b56b5c0bab91c1ce4b5ac43b59d28fb5cac0d9f9
dearjsmc4 <dearjsmc4@gmail.com>
2019-08-08 14:49:10 +0900

    Feat: Fix star-rate bug

--- src/app/store/components/store-detail-components/star-rate.component.ts
+++ src/app/store/components/store-detail-components/star-rate.component.ts
@@ -3,10 +3,10 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-star-rate',
   template: `
-  <div class="star-bg" [style.width.px]="width*1.1">
-    <div class="star-rate" [style.width.px]="starAvg * (width*1.1 / 5)">
+  <div class="star-bg" [style.width.px]="width">
+    <div class="star-rate" [style.width.px]="starAvg * (width / 5)">
       <img src="../../../../assets/image/star-rate.png" 
-        [style.width.px]="width*1.1">
+        [style.width.px]="width">
     </div> 
   </div>
 
