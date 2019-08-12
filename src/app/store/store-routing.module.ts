3cf39542d2688c2db56de15dd389cc2965e0d20c
hicucu <hicucu@gmail.com>
2019-08-12 16:34:29 +0900

    new page: brand, new component : list

--- src/app/store/store-routing.module.ts
+++ src/app/store/store-routing.module.ts
@@ -4,6 +4,7 @@ import { StoreComponent } from './components/store.component';
 import { CategoryComponent } from './components/category.component';
 import { RankComponent } from './components/rank.component';
 import { StoreDetailComponent } from './components/store-detail-components/store-detail.component';
+import { BrandComponent } from './components/brand.component';
 
 const routes: Routes = [
 
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'store/category', component: CategoryComponent },
   { path: 'store/rank', component: RankComponent },
   { path: 'store/:id', component: StoreDetailComponent }
+  { path: 'brand/:name', component: BrandComponent }
 ];
 
 @NgModule({
